39f37c99d530c32fc8a0f726ab452eab3d467218
atharva tamta <110754495+atharvatamta@users.noreply.github.com>
2022-08-30 17:40:51 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,13 +1,29 @@
-void selectionSort(vector<int>& arr, int n)
-{   
-    for(int i = 0; i < n; i++)
+#include <iostream>
+using namespace std;
+int selectSort(int arr[], int n)
+{
+  for(int i=0; i<n-1; i++) //number of rounds
+  {
+    int min=i;
+    for (int j = i+1; j<n; j++) //comparisions starting from i+1 since we considered i to be minimum
     {
-        int index = i;
-        for(int j = i+1; j < n; j++)
-        {
-            if(arr[j] < arr[index] )
-            index = j;    
-        }
-        swap(arr[index], arr[i]);
-    } 
+        if(arr[j]< arr[min])
+        min=j; //storing the index as minimum
+    }
+    swap(arr[min],arr[i]); //swaping the 'i'th element with the minimum element
+  }
+}
+void printArray(int arr[], int n) //printing the sorted array
+{
+    for (int i = 0; i < n; i++)
+    {
+      cout<<arr[i]<<" ";
+    }
+    
+}
+int main()
+{
+   int arr[5]={6, 2, 8, 4, 10 }; //put any sample array here 
+   selectSort(arr,5); 
+   printArray(arr,5);
 }
